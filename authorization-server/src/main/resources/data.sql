806054185e173f42ec632661e8a8898d54dceee0
emmanuel <emmanuel33700>
2019-10-05 13:30:45 +0200

    mise en place du socle

--- authorization-server/src/main/resources/data.sql
+++ authorization-server/src/main/resources/data.sql
@@ -3,21 +3,26 @@ insert into users (username, password, enabled, idAdherent) values ('bob', '$2a$
 insert into authorities (username, authority) values ('bob', 'ADHERENT');
 
 
-/* bob / mot de passe : 234 */
-insert into users (username, password, enabled, idAdherent) values ('sara', '$2a$10$WPDbKLCRnV0UrkEs2IEtUejsZiicxt0/GhUcOkg2.UscjBi8tOmxa', true, 40);
-insert into authorities (username, authority) values ('sara', 'CONSEIL');
-
-
-/* gege / mot de passe : 123 */
-insert into users (username, password, enabled, idAdherent) values ('gege', '$2a$10$/ns.CwZ9sdhQaVjw/bwBQeelnmTZTI19trLtyY/bjbIVUokAckX8y', true, 41);
-insert into authorities (username, authority) values ('gege', 'BUREAU');
-
-
-/* tete / mot de passe : 123 */
-insert into users (username, password, enabled, idAdherent) values ('tete', '$2a$10$/ns.CwZ9sdhQaVjw/bwBQeelnmTZTI19trLtyY/bjbIVUokAckX8y', true, 42);
-insert into authorities (username, authority) values ('tete', 'ADMIN');
-
-
 /* adherent@gmail.com / mot de passe : 123 */
 insert into users (username, password, enabled, idAdherent) values ('adherent@gmail.com', '$2a$10$/ns.CwZ9sdhQaVjw/bwBQeelnmTZTI19trLtyY/bjbIVUokAckX8y', true, 39);
 insert into authorities (username, authority) values ('adherent@gmail.com', 'ADHERENT');
+
+/* bureau@gmail.com / mot de passe : 123 */
+insert into users (username, password, enabled, idAdherent) values ('bureau@gmail.com', '$2a$10$/ns.CwZ9sdhQaVjw/bwBQeelnmTZTI19trLtyY/bjbIVUokAckX8y', true, 41);
+insert into authorities (username, authority) values ('bureau@gmail.com', 'BUREAU');
+insert into authorities (username, authority) values ('bureau@gmail.com', 'ADHERENT');
+
+/* conseil@gmail.com / mot de passe : 234 */
+insert into users (username, password, enabled, idAdherent) values ('conseil@gmail.com', '$2a$10$WPDbKLCRnV0UrkEs2IEtUejsZiicxt0/GhUcOkg2.UscjBi8tOmxa', true, 40);
+insert into authorities (username, authority) values ('conseil@gmail.com', 'CONSEIL');
+insert into authorities (username, authority) values ('conseil@gmail.com', 'BUREAU');
+insert into authorities (username, authority) values ('conseil@gmail.com', 'ADHERENT');
+
+
+/* admin@gmail.com / mot de passe : 123 */
+insert into users (username, password, enabled, idAdherent) values ('admin@gmail.com', '$2a$10$/ns.CwZ9sdhQaVjw/bwBQeelnmTZTI19trLtyY/bjbIVUokAckX8y', true, 42);
+insert into authorities (username, authority) values ('admin@gmail.com', 'ADMIN');
+insert into authorities (username, authority) values ('admin@gmail.com', 'CONSEIL');
+insert into authorities (username, authority) values ('admin@gmail.com', 'BUREAU');
+insert into authorities (username, authority) values ('admin@gmail.com', 'ADHERENT');
+
--- authorization-server/src/main/resources/schema.sql
+++ authorization-server/src/main/resources/schema.sql
@@ -1,7 +1,5 @@
 --------------- POSTGRESQL ---------------
-/*==============================================================*/
-/* Table : T_UTILISATEUR                                        */
-/*==============================================================*/
+
 
 CREATE TABLE IF NOT EXISTS  USERS(
 	username VARCHAR(50) not null primary key,
@@ -21,11 +19,3 @@ CREATE TABLE IF NOT EXISTS  AUTHORITIES (
 	constraint fk_authorities_users foreign key(username) references USERS(username)
 );
 
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE public.authorities TO GROUP authentification;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE public.users TO GROUP authentification;
-
-/*
-aide sur la création des droits dans PG ADMIN
-https://support.chartio.com/knowledgebase/creating-a-user-with-pgadmin
-
-*/
