b3bf55981f59b08c1f1a8d6168b4af61968faa4d
emmanuel <manu.chenais@gmail.com>
2020-04-03 13:50:31 +0200

    mise ne place socle front-end

--- front-end/website/src/framework/auth/components/auth.component.ts
+++ front-end/website/src/framework/auth/components/auth.component.ts
@@ -12,25 +12,8 @@ import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ngx-auth',
-  styleUrls: ['./auth.component.scss'],
   template: `
     <nb-layout>
-      <nb-layout-column>
-        <nb-card>
-          <nb-card-header>
-            <nav class="navigation">
-              <a href="#" (click)="back()" class="link back-link" aria-label="Back">
-                <nb-icon icon="arrow-back"></nb-icon>
-              </a>
-            </nav>
-          </nb-card-header>
-          <nb-card-body>
-            <nb-auth-block>
-              <router-outlet></router-outlet>
-            </nb-auth-block>
-          </nb-card-body>
-        </nb-card>
-      </nb-layout-column>
     </nb-layout>
   `,
 })
