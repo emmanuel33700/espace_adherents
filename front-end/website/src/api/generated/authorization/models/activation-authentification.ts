5b4202d43294130bf21d37a0677bbf2ea94b169a
emmanuel <manu.chenais@gmail.com>
2021-06-07 12:09:18 +0200

    mise en session info adh

---
+++ front-end/website/src/api/generated/authorization/models/activation-authentification.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+export interface ActivationAuthentification {
+
+  /**
+   * Indique le statut de l'authntification 
+   * * true => Le compte est activé
+   * * false => le compte est désactivé
+   */
+  statutActivation?: boolean;
+}
---
+++ front-end/website/src/api/generated/authorization/models/info-authentification.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+export interface InfoAuthentification {
+
+  /**
+   * Indique sur l'authentification est active
+   */
+  actif?: boolean;
+
+  /**
+   * id de la personne
+   */
+  idAdh?: number;
+
+  /**
+   * Login de la personnes
+   */
+  login?: string;
+}
