4e7477c267fffa33ea3cc8a79067979de3ad4374
emmanuel <manu.chenais@gmail.com>
2021-06-12 11:07:14 +0200

    mise ne place changement saisons

--- front-end/website/src/app/pages/saisons/liste/liste.component.ts
+++ front-end/website/src/app/pages/saisons/liste/liste.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {LoggerService} from '../../../@core/utils';
+import {NbToastrService} from '@nebular/theme';
+import {SaisonService} from '../../../../api/generated/utilitaire/services/saison.service';
+import {Saison} from '../../../../api/generated/utilitaire/models/saison';
+import {FormGroup, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'ngx-form-layouts',
   styleUrls: ['./liste.component.scss'],
   templateUrl: './liste.component.html',
 })
-export class ListeComponent {
+export class ListeComponent implements OnInit {
 
+  form: FormGroup ;
+
+  // Toaster
+  private index: number = 0;
+  @HostBinding('class')
+  classes = 'example-items-rows';
+  // fin toaster
+
+  // indicateur de chargement
+  loading = true;
+
+  saisons: Saison[] = [];
+
+  selectedOption = 0;
+
+  constructor(    private loggerService: LoggerService,
+                  private toastrService: NbToastrService,
+                  private saisonService: SaisonService,
+
+  ) {
+  }
+
+  /**
+   * Initialisation des saisions
+   */
+  ngOnInit(): void {
+
+    // récupéer la liste des saisions
+    this.saisonService.getListeSaison({}).subscribe(
+      (data) => {
+        this.saisons = data;
+        this.loggerService.debug(JSON.stringify(data));
+
+        this.loading = false;
+      },
+      (error) => {
+        this.loggerService.error(error);
+        this.toastrService.danger(
+          'Erreur technique lors de le recupération des informations',
+          'Erreur ');
+      },
+      () => {
+        this.loggerService.debug('fini');
+
+        this.loading = false;
+      });
+
+  }
+
+  /**
+   * Validation du formulaire
+   * @param form
+   */
+  submit(form: NgForm) {
+
+    if (this.selectedOption !== 0) {
+      this.loggerService.info('Changement de saisie avec ID ' + this.selectedOption);
+
+      this.saisonService.updateSaisonCourante({idSaison: this.selectedOption})
+        .subscribe(
+          (data) => {
+            this.loggerService.info(JSON.stringify(data));
+          },
+          (error) => {
+            this.loggerService.error(error);
+            this.toastrService.danger(
+              'Erreur technique lors de enregistrement',
+              'Erreur ');
+          },
+          () => {
+            this.loggerService.info('MàJ OK');
+            localStorage.setItem('id_annee_adhesion', String(this.selectedOption));
+            this.toastrService.success(
+              'Mise à jour finalisée',
+              'Opértation réussit');
+          },
+        );
+    }
+
+
+  }
 }
