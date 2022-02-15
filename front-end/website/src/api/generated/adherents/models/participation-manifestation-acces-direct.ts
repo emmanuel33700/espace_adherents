0ff52f5150cf90fa3182000f5af93861bf7da5ab
emmanuel <manu.chenais@gmail.com>
2022-02-15 17:25:18 +0100

    [FRONT-END] - ajout export liste adhérent et évolution gestion des mailing liste

---
+++ front-end/website/src/api/generated/adherents/models/participation-manifestation-acces-direct.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+export interface ParticipationManifestationAccesDirect {
+  emailadherent?: string;
+
+  /**
+   * Types de participation. Valeurs possibles :
+   * * 1 : PARTICIPE               
+   * * 2 : PARTICIPE PAS
+   */
+  statutParticipation?: 1 | 2;
+}
