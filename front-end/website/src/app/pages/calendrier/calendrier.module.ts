3e47480f44637e05813e00e0f75648170090d1d7
emmanuelchenais <manu.chenais@gmail.com>
2024-02-24 23:06:48 +0100

    FRONTEND - Netoyage du code sur la gestion du calendrier

--- front-end/website/src/app/pages/calendrier/calendrier.module.ts
+++ front-end/website/src/app/pages/calendrier/calendrier.module.ts
@@ -20,20 +20,10 @@ import { SyntheseparticipationComponent } from './syntheseparticipation/synthese
 import { SyntheseevenementComponent } from './syntheseevenement/syntheseevenement.component';
 import { FormsModule as ngFormsModule } from '@angular/forms';
 import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction'; // a plugin
-import listPlugin from '@fullcalendar/list';
 import {DialogDetailEvenementComponent} from './liste/dialog-detail-evenement/dialog-detail-evenement.component';
 import {DialogAjoutEvenementComponent} from './liste/dialog-ajout-evenement/dialog-ajout-evenement.component';
 
-/** FullCalendarModule.registerPlugins([ // register FullCalendar plugins
-  dayGridPlugin,
-  timeGridPlugin,
-  listPlugin,
-  interactionPlugin,
-]);
-*/
+
 @NgModule({
     imports: [
         ThemeModule,
